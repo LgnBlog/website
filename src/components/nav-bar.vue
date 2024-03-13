cabc190ce027d6866ae8fa3bfdd4baa3733dbaf8
Li GuangNing <lign@stpass.com>
2024-03-13 18:30:58 +0800

    基础组件提交

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,27 +1,21 @@
 <!--
  * @Author: Li GuangNing lign@stpass.com
- * @Date: 2024-03-13 17:28:50
+ * @Date: 2024-03-13 18:24:19
  * @LastEditors: Li GuangNing lign@stpass.com
- * @LastEditTime: 2024-03-13 18:30:33
- * @FilePath: \website\src\views\homePage.vue
+ * @LastEditTime: 2024-03-13 18:24:40
+ * @FilePath: \website\src\components\nav.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div class="navbar">
-    <navBar></navBar>
-  </div>
-  <div class="content">
-    <contentComponent></contentComponent>
+  <div>
+    nav
   </div>
 </template>
 
 <script setup>
-import navBar from '@/components/nav-bar.vue';
-import contentComponent from '@/components/content-component.vue';
 
 </script>
 
 <style lang="scss" scoped>
-   
 
 </style>
